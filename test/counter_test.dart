7434c638620ac9f94cca58cf1811c1fa5afa14cb
Pierry Borges <_pierry.borges.ext@pluxeegroup.com>
2025-02-03 00:40:51 -0300

    :white_check_mark: add test

---
+++ test/counter_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_architectures/mvvm/view/counter_view.dart';
+import 'package:flutter_architectures/mvvm/viewmodels/counter_view_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_architectures/main.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    final viewModel = CounterViewModel();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ChangeNotifierProvider<CounterViewModel>.value(
+          value: viewModel,
+          child: CounterViewPage(),
+        ),
+      ),
+    );
+
+    expect(find.text('Count: 0'), findsOneWidget);
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pump();
+
+    expect(find.text('Count: 1'), findsOneWidget);
+  });
+}
